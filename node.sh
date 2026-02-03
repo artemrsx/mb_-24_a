f415e198dc70d489422b77ac73e90534df1e3b1b
Bochkarev Artem <artbochkarev@ozon.ru>
2026-02-03 13:41:52 +0300

    update

--- node.sh
+++ node.sh
@@ -3,30 +3,42 @@
 NODE_NAME=$1
 CERTIFICATE=$2
 
-# 1. Проверка входных данных (чтобы не затереть конфиги пустыми данными)
+# Проверка входных данных (чтобы не затереть конфиги пустыми данными)
 if [ -z "$NODE_NAME" ] || [ -z "$CERTIFICATE" ]; then
   echo "Usage: $0 <node_name> '<certificate_content>'"
   exit 1
 fi
 
-# 2. Обновляем сервер и софт
+# Обновляем сервер и софт
 apt-get update && apt-get upgrade -y
 apt-get install curl socat git -y
 
-# 3. Быстрая установка node
-sudo bash -c "$(curl -sL https://github.com/Gozargah/Marzban-scripts/raw/master/marzban-node.sh)" @ install --name "$NODE_NAME"
+# Быстрая установка node
+# sudo bash -c "$(curl -sL https://github.com/Gozargah/Marzban-scripts/raw/master/marzban-node.sh)" @ install --name "$NODE_NAME"
 
-# 4. Создаем файл для сертификата
+# 1. Сертификат
+# 2. Enter (завершить ввод сертификата)
+# 3. y (согласиться на REST)
+# 4. Enter (согласиться на service port по умолчанию 62050)
+# 5. Enter (согласиться на xray port по умолчанию 62051)
+printf "$CERTIFICATE\n\ny\n\n\n" | sudo bash -c "$(curl -sL https://github.com)" @ install --name "$NODE_NAME"
+
+# Создаем файл для сертификата
 sudo mkdir -p /var/lib/marzban-node/
 
-# 5. Записываем сертификат
+# Записываем сертификат
 sudo tee /var/lib/marzban-node/ssl_client_cert.pem <<EOF > /dev/null
 $CERTIFICATE
 EOF
 
-# 6. Прастим путь в docker-compose.yml автоматически
+# Прастим путь в docker-compose.yml автоматически
 # Скрипт установки может создать файл с дефолтными путями, лучше их перепроверить/заменить
 sed -i 's|SSL_CLIENT_CERT_FILE:.*|SSL_CLIENT_CERT_FILE: "/var/lib/marzban-node/ssl_client_cert.pem"|' /opt/marzban-node/docker-compose.yml
 
-# 7. Перезапускаем ноду
+# На всякий случай открываем нужные порты (по дефолту они и так отрыты)
+ufw allow 62050/tcp
+ufw allow 62051/tcp
+ufw allow 443/tcp
+
+# Перезапускаем ноду
 cd /opt/marzban-node && docker compose down && docker compose up -d
